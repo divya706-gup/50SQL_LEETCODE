53f44c417adb1c73c9103316a1b4f749450c365a
divya gupta <divyagupta70600@gmail.com>
2026-07-19 13:55:58 +0530

    🟡 Add 1193. Monthly Transactions I [Medium] (MySQL)

---
+++ Medium/1193-Monthly-Transactions-I/solution.sql
@@ -0,0 +1,23 @@
+-- ═══════════════════════════════════════════════════════
+--  Problem  : 1193. Monthly Transactions I
+--  URL      : https://leetcode.com/problems/monthly-transactions-i/submissions/2073246488/?envType=study-plan-v2&envId=top-sql-50
+--  Difficulty : Medium
+--  Language : MySQL
+--  Runtime  : 547 ms
+--  Memory   : 0B
+--  Solved   : July 19, 2026
+-- ═══════════════════════════════════════════════════════
+
+# Write your MySQL query statement below
+SELECT 
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    country,
+    COUNT(id) AS trans_count,
+    SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM 
+    Transactions
+GROUP BY 
+    DATE_FORMAT(trans_date, '%Y-%m'), 
+    country;
